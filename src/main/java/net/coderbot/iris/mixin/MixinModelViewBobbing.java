27e9b74a7ff7c24e9105441f295b140d4e30c996
Asek3 <64613894+Asek3@users.noreply.github.com>
2023-09-07 19:21:06 +0300

    Do not apply modelview bobbing with shaders off
    Co-Authored-By: IMS <ims@irisshaders.dev>

--- src/main/java/net/coderbot/iris/mixin/MixinModelViewBobbing.java
+++ src/main/java/net/coderbot/iris/mixin/MixinModelViewBobbing.java
@@ -1,5 +1,6 @@
 package net.coderbot.iris.mixin;
 
+import net.irisshaders.iris.api.v0.IrisApi;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -27,10 +28,20 @@ public class MixinModelViewBobbing {
 	@Unique
 	private Matrix4f bobbingEffectsModel;
 
+	@Unique
+	private boolean areShadersOn;
+
+	@Inject(method = "renderLevel", at = @At("HEAD"))
+	private void iris$saveShadersOn(float pGameRenderer0, long pLong1, PoseStack pPoseStack2, CallbackInfo ci) {
+		areShadersOn = IrisApi.getInstance().isShaderPackInUse();
+	}
+
 	@ModifyArg(method = "renderLevel", index = 0,
 			at = @At(value = "INVOKE",
 					target = "Lnet/minecraft/client/renderer/GameRenderer;bobHurt(Lcom/mojang/blaze3d/vertex/PoseStack;F)V"))
 	private PoseStack iris$separateViewBobbing(PoseStack stack) {
+		if (!areShadersOn) return stack;
+
 		stack.pushPose();
 		stack.last().pose().setIdentity();
 
@@ -43,6 +54,8 @@ public class MixinModelViewBobbing {
 			slice = @Slice(from = @At(value = "INVOKE",
 					       target = "Lnet/minecraft/client/renderer/GameRenderer;bobHurt(Lcom/mojang/blaze3d/vertex/PoseStack;F)V")))
 	private PoseStack.Pose iris$saveBobbing(PoseStack stack) {
+		if (!areShadersOn) return stack.last();
+
 		bobbingEffectsModel = stack.last().pose().copy();
 
 		stack.popPose();
@@ -54,6 +67,8 @@ public class MixinModelViewBobbing {
 			at = @At(value = "INVOKE",
 					target = "Lnet/minecraft/client/renderer/GameRenderer;resetProjectionMatrix(Lcom/mojang/math/Matrix4f;)V"))
 	private void iris$applyBobbingToModelView(float tickDelta, long limitTime, PoseStack matrix, CallbackInfo ci) {
+		if (!areShadersOn) return;
+
 		matrix.last().pose().multiply(bobbingEffectsModel);
 
 		bobbingEffectsModel = null;
