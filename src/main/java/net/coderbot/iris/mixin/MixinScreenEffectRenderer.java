5b1c57e93ba6d3f0987005e3f6f9a25c7442acb0
Asek3 <64613894+Asek3@users.noreply.github.com>
2023-09-21 21:52:36 +0300

    Fixed some fluid overlays
    related to https://github.com/MinecraftForge/MinecraftForge/blob/1.19.2/patches/minecraft/net/minecraft/client/renderer/ScreenEffectRenderer.java.patch#L57

--- src/main/java/net/coderbot/iris/mixin/MixinScreenEffectRenderer.java
+++ src/main/java/net/coderbot/iris/mixin/MixinScreenEffectRenderer.java
@@ -5,6 +5,7 @@ import net.coderbot.iris.Iris;
 import net.coderbot.iris.pipeline.WorldRenderingPipeline;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ScreenEffectRenderer;
+import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,8 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ScreenEffectRenderer.class)
 public abstract class MixinScreenEffectRenderer {
-	@Inject(method = "renderWater", at = @At(value = "HEAD"), cancellable = true)
-	private static void iris$disableUnderWaterOverlayRendering(Minecraft minecraft, PoseStack poseStack, CallbackInfo ci) {
+	@Inject(method = "renderFluid", at = @At(value = "HEAD"), cancellable = true, remap = false)
+	private static void iris$disableUnderWaterOverlayRendering(Minecraft minecraft, PoseStack poseStack, ResourceLocation texture, CallbackInfo ci) {
 		WorldRenderingPipeline pipeline = Iris.getPipelineManager().getPipelineNullable();
 
 		if (pipeline != null && !pipeline.shouldRenderUnderwaterOverlay()) {
