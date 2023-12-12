12411bf9530939be427c2440411c2cc4369084d7
Asek3 <64613894+Asek3@users.noreply.github.com>
2023-12-12 20:36:07 +0200

    minor

--- src/main/java/net/coderbot/iris/mixin/compat/pixelmon/MixinNormalizedFace.java
+++ src/main/java/net/coderbot/iris/mixin/compat/pixelmon/MixinNormalizedFace.java
@@ -46,14 +46,6 @@ public class MixinNormalizedFace {
             Vector4f transformedPosition = pose.transform(new Vector4f(vertex.getX(partialTick), vertex.getY(partialTick), vertex.getZ(partialTick), 1.0F));
             Vector3f transformedNormal = normal.transform(new Vector3f(vertex.getXN(partialTick), vertex.getYN(partialTick), vertex.getZN(partialTick)));
             bufferBuilder.vertex(transformedPosition.x(), transformedPosition.y(), transformedPosition.z(), r, g, b, a, this.textureCoordinates[i].u, this.textureCoordinates[i].v, packedOverlay, packedLight, transformedNormal.x(), transformedNormal.y(), transformedNormal.z());
-            /*bufferBuilder.vertex(pose.m00() * this.vertices[i]
-                    .getX(partialTick) + pose.m01() * this.vertices[i].getY(partialTick) + pose.m02() * this.vertices[i].getZ(partialTick) + pose.m03(), pose.m10() * this.vertices[i]
-                    .getX(partialTick) + pose.m11() * this.vertices[i].getY(partialTick) + pose.m12() * this.vertices[i].getZ(partialTick) + pose.m13(), pose.m20() * this.vertices[i]
-                    .getX(partialTick) + pose.m21() * this.vertices[i].getY(partialTick) + pose.m22() * this.vertices[i].getZ(partialTick) + pose.m23(), r, g, b, a, (this.textureCoordinates[i]).u, (this.textureCoordinates[i]).v, packedOverlay, packedLight, normal.m00 * this.vertices[i]
-
-                    .getXN(partialTick) + normal.m01 * this.vertices[i].getYN(partialTick) + normal.m02 * this.vertices[i].getZN(partialTick), normal.m10 * this.vertices[i]
-                    .getXN(partialTick) + normal.m11 * this.vertices[i].getYN(partialTick) + normal.m12 * this.vertices[i].getZN(partialTick), normal.m20 * this.vertices[i]
-                    .getXN(partialTick) + normal.m21 * this.vertices[i].getYN(partialTick) + normal.m22 * this.vertices[i].getZN(partialTick));*/
         }
     }
 
