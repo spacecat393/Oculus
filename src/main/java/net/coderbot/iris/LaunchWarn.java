7b84f0d7d5fb75f6e62abad59b33e32ab72e0b74
Asek3 <64613894+Asek3@users.noreply.github.com>
2024-03-19 09:50:17 +0200

    Ported LaunchWarn changes from 1.20

--- src/main/java/net/coderbot/iris/LaunchWarn.java
+++ src/main/java/net/coderbot/iris/LaunchWarn.java
@@ -12,8 +12,8 @@ import javax.swing.UnsupportedLookAndFeelException;
 public class LaunchWarn {
 	public static void main(String[] args) {
 		// TODO: make this translatable
-		String message = "This file is the Fabric version of Iris, meant to be installed as a mod. Would you like to get the Iris Installer instead?";
-		String fallback = "This file is the Fabric version of Iris, meant to be installed as a mod. Please download the Iris Installer from https://irisshaders.net.";
+		String message = "This file is the Forge version of Oculus, meant to be installed as a mod. Would you like to get the Forge Installer instead?";
+		String fallback = "This file is the Forge version of Oculus, meant to be installed as a mod. Please download the Forge Installer from https://files.minecraftforge.net";
 		if (GraphicsEnvironment.isHeadless()) {
 			System.err.println(fallback);
 		} else {
@@ -24,11 +24,11 @@ public class LaunchWarn {
 			}
 
 			if (Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-				int option = JOptionPane.showOptionDialog(null, message, "Iris Installer", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
+				int option = JOptionPane.showOptionDialog(null, message, "Oculus Installer", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
 
 				if (option == JOptionPane.YES_OPTION) {
 					try {
-						Desktop.getDesktop().browse(URI.create("https://irisshaders.net"));
+						Desktop.getDesktop().browse(URI.create("https://files.minecraftforge.net"));
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
