840aacdccb6f84dd1d0a730e920196d5fe359670
dima_dencep <dima_dencep@constructlegacy.ru>
2024-04-29 02:19:37 +0700

    just because i can
    Co-Authored-By: Bogdan <64613894+Asek3@users.noreply.github.com>

---
+++ src/desktop/java/net/irisshaders/iris/DesktopBuildConfig.java
@@ -0,0 +1,5 @@
+package net.irisshaders.iris;
+
+public class DesktopBuildConfig {
+    public static final boolean IS_SHARED_BETA = false;
+}
--- src/desktop/java/net/irisshaders/iris/LaunchWarn.java
+++ src/desktop/java/net/irisshaders/iris/LaunchWarn.java
@@ -9,11 +9,11 @@ public class LaunchWarn {
 	public static void main(String[] args) {
 		// TODO: make this translatable
 		String message = DesktopBuildConfig.IS_SHARED_BETA
-			? "If you're seeing this, you didn't read instructions.\n (Hint: This isn't a installer or a Forge mod. It's a Fabric mod.)"
-			: "This file is the Fabric version of Iris, meant to be installed as a mod. Would you like to get the Iris Installer instead?";
+			? "If you're seeing this, you didn't read instructions.\n (Hint: This isn't a installer or a Fabric mod. It's a (Neo)Forge mod.)"
+			: "This file is the Forge version of Oculus, meant to be installed as a mod. Would you like to get the NeoForge Installer instead?";
 		String fallback = DesktopBuildConfig.IS_SHARED_BETA
-			? "If you're seeing this, you didn't read instructions.\n (Hint: This isn't a installer or a Forge mod. It's a Fabric mod.)"
-			: "This file is the Fabric version of Iris, meant to be installed as a mod. Please download the Iris Installer from https://irisshaders.dev.";
+			? "If you're seeing this, you didn't read instructions.\n (Hint: This isn't a installer or a Fabric mod. It's a (Neo)Forge mod.)"
+			: "This file is the Forge version of Oculus, meant to be installed as a mod. Please download the NeoForge Installer from https://neoforged.net.";
 		if (GraphicsEnvironment.isHeadless()) {
 			System.err.println(fallback);
 		} else {
@@ -24,11 +24,11 @@ public class LaunchWarn {
 			}
 
 			if (Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-				int option = JOptionPane.showOptionDialog(null, message, "Iris Installer", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
+				int option = JOptionPane.showOptionDialog(null, message, "Oculus Installer", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
 
 				if (option == JOptionPane.YES_OPTION) {
 					try {
-						Desktop.getDesktop().browse(URI.create("https://irisshaders.dev"));
+						Desktop.getDesktop().browse(URI.create("https://neoforged.net"));
 					} catch (IOException e) {
 						System.out.println("Welp; we're screwed.");
 						e.printStackTrace();
