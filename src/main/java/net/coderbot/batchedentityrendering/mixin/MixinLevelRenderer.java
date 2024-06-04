43b9579c1cba8fdb8a9e7cf8b9664ea43bd26acf
Riley Nielsen <rileyjnielsen@gmail.com>
2024-06-03 19:38:31 -0700

    update MixinLevelRenderer

--- src/main/java/net/coderbot/batchedentityrendering/mixin/MixinLevelRenderer.java
+++ src/main/java/net/coderbot/batchedentityrendering/mixin/MixinLevelRenderer.java
@@ -1,5 +1,6 @@
 package net.coderbot.batchedentityrendering.mixin;
 
+import net.minecraft.util.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -8,74 +9,65 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-//import com.mojang.math.Matrix4f;
-import net.coderbot.iris.vendored.joml.Matrix4f;
-
 import net.coderbot.batchedentityrendering.impl.DrawCallTrackingRenderBuffers;
 import net.coderbot.batchedentityrendering.impl.Groupable;
 import net.coderbot.batchedentityrendering.impl.RenderBuffersExt;
-import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.LightTexture;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderBuffers;
+import net.minecraft.client.renderer.EntityRenderer;
+import net.minecraft.client.renderer.RenderGlobal;
+import net.minecraft.entity.Entity;
 
 /**
- * Tracks whether or not the world is being rendered, and manages grouping
+ * Tracks whether the world is being rendered, and manages grouping
  * with different entities.
  */
 // Uses a priority of 999 to apply before the main Iris mixins to draw entities before deferred runs.
-@Mixin(value = LevelRenderer.class, priority = 999)
+@Mixin(value = RenderGlobal.class, priority = 999)
 public class MixinLevelRenderer {
 	private static final String RENDER_ENTITY =
-			"Lnet/minecraft/client/renderer/LevelRenderer;renderEntity(Lnet/minecraft/world/entity/Entity;DDDFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;)V";
+			"renderEntity(Lnet/minecraft/entity/Entity;DDDFFZ)V";
 
 	@Shadow
-	private RenderBuffers renderBuffers;
+	private EntityRenderer entityRenderer;
 
 	@Unique
 	private Groupable groupable;
 
 	@Inject(method = "renderLevel", at = @At("HEAD"))
-	private void batchedentityrendering$beginLevelRender(PoseStack poseStack, float f, long l, boolean bl, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f matrix4f, CallbackInfo ci) {
-		if (renderBuffers instanceof DrawCallTrackingRenderBuffers) {
-			((DrawCallTrackingRenderBuffers) renderBuffers).resetDrawCounts();
+	private void batchedentityrendering$beginLevelRender(Entity entity, double d0, double d1, double d2, float partialTicks, CallbackInfo ci) {
+		if (entityRenderer instanceof DrawCallTrackingRenderBuffers) {
+			((DrawCallTrackingRenderBuffers) entityRenderer).resetDrawCounts();
 		}
-
-		((RenderBuffersExt) renderBuffers).beginLevelRendering();
-		MultiBufferSource provider = renderBuffers.bufferSource();
-
-		if (provider instanceof Groupable) {
-			groupable = (Groupable) provider;
+		((RenderBuffersExt) entityRenderer).beginLevelRendering();
+		if (entityRenderer instanceof Groupable) {
+			groupable = (Groupable) entityRenderer;
 		}
 	}
 
 	@Inject(method = "renderLevel", at = @At(value = "INVOKE", target = RENDER_ENTITY))
-	private void batchedentityrendering$preRenderEntity(PoseStack poseStack, float f, long l, boolean bl, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f matrix4f, CallbackInfo ci) {
+	private void batchedentityrendering$preRenderEntity(Entity entity, double d0, double d1, double d2, float partialTicks, CallbackInfo ci) {
 		if (groupable != null) {
 			groupable.startGroup();
 		}
 	}
 
 	@Inject(method = "renderLevel", at = @At(value = "INVOKE", target = RENDER_ENTITY, shift = At.Shift.AFTER))
-	private void batchedentityrendering$postRenderEntity(PoseStack poseStack, float f, long l, boolean bl, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f matrix4f, CallbackInfo ci) {
+	private void batchedentityrendering$postRenderEntity(Entity entity, double d0, double d1, double d2, float partialTicks, CallbackInfo ci) {
 		if (groupable != null) {
 			groupable.endGroup();
 		}
 	}
 
 	@Inject(method = "renderLevel", at = @At(value = "CONSTANT", args = "stringValue=translucent"), locals = LocalCapture.CAPTURE_FAILHARD)
-	private void batchedentityrendering$beginTranslucents(PoseStack poseStack, float f, long l, boolean bl, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f matrix4f, CallbackInfo ci) {
-		Minecraft.getInstance().getProfiler().popPush("entity_draws");
-		this.renderBuffers.bufferSource().endBatch();
+	private void batchedentityrendering$beginTranslucents(Entity entity, double d0, double d1, double d2, float partialTicks, CallbackInfo ci) {
+		Minecraft.getMinecraft().profiler.endStartSection("entity_draws");
+		// todo: double-check
+		Minecraft.getMinecraft().getRenderManager().renderEngine.bindTexture(new ResourceLocation("textures/entity/default.png"));
 	}
 
 	@Inject(method = "renderLevel", at = @At("RETURN"))
-	private void batchedentityrendering$endLevelRender(PoseStack poseStack, float f, long l, boolean bl, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f matrix4f, CallbackInfo ci) {
-		((RenderBuffersExt) renderBuffers).endLevelRendering();
+	private void batchedentityrendering$endLevelRender(Entity entity, double d0, double d1, double d2, float partialTicks, CallbackInfo ci) {
+		((RenderBuffersExt) entityRenderer).endLevelRendering();
 		groupable = null;
 	}
 }
